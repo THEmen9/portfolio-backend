a6ace0a3b05483cc0cdc4f9193dff0cc79d106f0
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-08-11 19:05:38 +0530

    feat: add contact form POST route

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import { projectsData, aboutContent } from './data.js';
     
 
 const app = express();
+
+// --------middlewares----------//
 app.use(cors());
+app.use(express.json())
 
 //-----------------------Routes---------------------------//
 
@@ -16,7 +19,7 @@ app.get('/api/projects', (req, res) => {
 
 //Single-Project-byId--------//
 app.get('/api/projects/:id', (req, res) => {
-  const id = req.params.id; // ye string hoga, "1", "2" etc.
+  const id = req.params.id; 
   const project = projectsData.find((p) => p.id === Number(id));
     if(!project) {
       return res.status(404).json({error: 'Project not found' })
@@ -28,6 +31,22 @@ app.get('/api/projects/:id', (req, res) => {
 //About------------//
 app.get('/api/about', (req, res) => {
     res.json(aboutContent);
+});
+
+// ---------------POST route----------------//
+app.post('/api/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  
+  if (!name || !email || !message) { 
+    return res.status(400).json({ 
+      error: 'All fields required' 
+    });
+   } 
+
+   res.status(200).json({
+    message: 'Message received successfully'
+  });
+
 });
 
 app.listen(5000, () => {
