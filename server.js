fe85612654c03a353fcc3ab5a13148bd8f96f447
Ankit Kumar <122370470+THEmen9@users.noreply.github.com>
2026-09-03 18:11:02 +0530

    refactore: syntax arrangement

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 // db import
 import connectDB from './conf/db.js';
+
 // routes import 
 import projectRoutes from './routes/projects.js';
 import contactRoutes from './routes/contact.js';
 import aboutRoutes from './routes/about.js';
 import adminRoutes from './routes/admin.js'
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 // --------middlewares----------//
 app.use(cors({
   origin: 'http://localhost:5173',
